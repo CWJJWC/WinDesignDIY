430d880a5f62a1641c4810531af06c0141f61fc1
CWJJWC <cwjjwc849@sina.com>
2017-03-03 10:34:56 +0800

    WinDesignDevelop

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+	Camera myCam;
+	public float speed = 1.0f;
+	public float horizontalSpeed = 2.0f;
+	public float verticalSpeed = 2.0f;
+	public float minDis = 15f;
+	public float maxDis = 120f;
+	public float sensitivity = 10f;
+
+
+	Vector3 rotateTarget;
+	Vector3 currentEuler;
+	Vector3 rotateVelocity;
+
+	// Use this for initialization
+	void Start () {
+		myCam = Camera.main;
+		rotateTarget = myCam.transform.eulerAngles;
+		currentEuler = myCam.transform.eulerAngles;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			myCam.transform.Translate (Vector3.forward * speed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			myCam.transform.Translate (Vector3.left * speed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			myCam.transform.Translate (Vector3.back * speed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			myCam.transform.Translate (Vector3.right * speed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetMouseButton(0)) {
+			
+		}
+		if (Input.GetMouseButton(1)) {
+			float inputx = -horizontalSpeed * Input.GetAxis ("Mouse Y");
+			float inputY = verticalSpeed * Input.GetAxis ("Mouse X");
+			rotateTarget += new Vector3 (inputx,inputY,0);
+			rotateTarget.z = 0;
+//			myCam.transform.Rotate (inputx, inputY, 0,Space.World);
+			currentEuler = rotateTarget;
+			myCam.transform.eulerAngles = currentEuler;
+		}
+		if (Input.GetMouseButton(2)) {
+			float inputx = -horizontalSpeed * Input.GetAxis ("Mouse X");
+			float inputY = -verticalSpeed * Input.GetAxis ("Mouse Y");
+			myCam.transform.Translate (inputx*horizontalSpeed*Time.deltaTime, inputY*verticalSpeed*Time.deltaTime, 0,Space.Self);
+		}
+
+		if (Input.GetAxis ("Mouse ScrollWheel") != 0) {
+			float distance = Input.GetAxis("Mouse ScrollWheel") * 5;
+			myCam.transform.Translate (0, 0, distance, Space.Self);
+//			myCam.transform.localPosition= new Vector3(myCam.transform.localPosition.x,myCam.transform.localPosition.y, distance);
+
+		}
+	}
+}
---
+++ Assets/Scripts/SelectObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class SelectObject : MonoBehaviour {
+	GameObject temp ;
+
+
+	void Update(){
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			LayerMask mask = ~(1<<LayerMask.NameToLayer ("Floor"));
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 100f, mask.value)) {
+//				print ("hhhhhhh");
+//				if (hit.collider.gameObject.GetComponent<Outline> ()) {
+//					hit.collider.gameObject.GetComponent<Outline> ().enabled = true;
+//					temp = hit.collider.gameObject;
+//				}
+				DelTemp();
+				AddTemp (hit);
+			} else {
+//				if (temp&&temp.GetComponent<Outline> ()) {
+//					temp.GetComponent<Outline> ().enabled = false;
+//				}
+				DelTemp();
+			}
+
+		}
+	}
+
+	void AddTemp(RaycastHit hit){
+		
+		temp = hit.collider.gameObject;
+		Renderer[] allChild = temp.GetComponentsInChildren<Renderer> ();
+		foreach (Renderer child in allChild) {
+			if (child.gameObject.GetComponent<Outline> ()) {
+				child.gameObject.GetComponent<Outline> ().enabled = true;
+			}
+		}
+	}
+
+	void DelTemp(){
+		if (temp == null) {
+			return;
+		}
+		Renderer[] allChild = temp.GetComponentsInChildren<Renderer> ();
+		foreach (Renderer child in allChild) {
+			if (child.gameObject.GetComponent<Outline> ()) {
+				child.gameObject.GetComponent<Outline> ().enabled = false;
+			}
+		}
+		temp = null;
+	}
+}
